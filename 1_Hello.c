5acc40f58e3032ca43fce0ad3a33612610316fab
Pranoy Bhan <76389977+Pranoy-Bhan@users.noreply.github.com>
2021-03-18 21:35:40 +0530

    Update 1_Hello.c

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
+#include<stdio.h>	//Preprocessor directive to include standard input output header file
 int main()
 {
-	printf("HELLO WORLD");
+	printf("HELLO WORLD");	//printing hello world
 
 	return 0;
 }
