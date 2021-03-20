8b77df0c1effadcdd745ea5605e2d7d130740436
Pranoy Bhan <76389977+Pranoy-Bhan@users.noreply.github.com>
2021-03-20 10:57:20 +0530

    Update 1_Hello.c

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,7 +1,13 @@
+//Author Name: Pranoy Bhan
+//Creation Date: 16 March 2021
+//Purpose: To print hello world program
+
 #include<stdio.h>	//Preprocessor directive to include standard input output header file
-int main()
-{
+int main()	
+{	//Start of main body
+	
 	printf("HELLO WORLD");	//printing hello world
 
-	return 0;
+	return 0;	//Return Statement
 }
+//End of main body
