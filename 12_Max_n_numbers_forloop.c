7d5ce5b7f3914826082d71f37e4af66c34789b1f
Pranoy-Bhan <2020a1r114@mietjammu.in>
2021-03-22 22:22:50 +0530

    file added

---
+++ 12_Max_n_numbers_forloop.c
@@ -0,0 +1,24 @@
+//Author name: Pranoy Bhan
+//Creation date: 21 March 2021
+//Purpose: Maximum of n numbers using for loop
+
+#include <stdio.h>	//Preprocessor directive to include standard input output function header file
+int main()
+{	//start of the main body function
+  int i,num,n,large=0;	//Declaring variables of integer data type
+
+  printf("How many numbers: ");	//Printf function calling to print how many numbers
+  scanf("%d",&n); //Scanf function calling to take input from user 
+  
+  for(i=0; i<n; i++)
+  {	//for loop body starts
+    printf("\nEnter number %d: ",i+1);	//prinf function calling to enter number with i+1
+    scanf("%d",&num);	//Scanf function calling to take input from user
+    if(num>large)	//using if statement
+    large=num;	//large is assigned to num
+  }	//for loop body ends
+
+  printf("\nThe Largest number is %d",large);	//Printf function calling to print The Largest number
+  
+  return 0;	//return statement
+}//end of main function body
