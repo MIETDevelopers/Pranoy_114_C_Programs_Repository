4a9748eb0c24a4af75b06c021786fc8de6072856
Pranoy-Bhan <2020a1r114@mietjammu.in>
2021-03-22 21:33:37 +0530

    file updated

--- factorial_forloop.c
+++ factorial_forloop.c
@@ -1,23 +1,21 @@
-#include <stdio.h>
-int main() 
-{
-    int num, i;
-    
-	unsigned long long fact = 1;
-    printf("Enter an integer: ");
-    scanf("%d", &num);
+//Author Name: Pranoy Bhan
+//Creation Date: 20 March 2021
+//Purpose: To find the Factorial of a Number using for loop 
 
-    
-    if (num < 0)	// shows error if the user enters a negative integer
-        printf("Error! Factorial of a negative number doesn't exist.");
-    else 
-	{
-        for (i = 1; i <= num; ++i)
-	{
-        fact *= i;
-    }
-        printf("Factorial of %d = %llu", num, fact);
-    }
-
-    return 0;
-}
+#include<stdio.h>	//Preprocessor directive to include input output function header file
+int main()
+{	//Start of main body
+	
+	int n,i,f;	//Declaring variables of integer data type
+	f=1;	//value assignment to variable
+	
+	printf("Enter a Number : ");	//printf function calling to print Number
+	scanf("%d",&n);	//scanf function calling for user input
+	for (i=1; i<=n; i++)
+	{	//start of function body
+		
+		f=f*i;	//factorial formula
+	}
+	printf("%d",f);	//printf function calling to print factorial
+	return 0;	//Return statement
+}//End of main function body
