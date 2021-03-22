08656c39979ea32e70058ef1a9ea06f6f12c6bee
Pranoy-Bhan <2020a1r114@mietjammu.in>
2021-03-23 01:04:47 +0530

    file added

---
+++ 26_Calloc.c
@@ -0,0 +1,50 @@
+//Author:Pranoy Bhan
+//Creation Date: 22 March 2021
+//Purpose:To demonstrate the use of Calloc()
+
+#include <stdio.h> //Preprocessor directive to include input output functions header file
+#include <stdlib.h> //Preprocessor directive to include standard library header file
+int main()
+{ //Start of the main body function
+  
+    // This pointer will hold the 
+    // base address of the block created 
+    int* ptr; //pointer declarartion
+    int n, i; //variable declarartion
+  
+    // Get the number of elements for the array 
+    n = 5; // value assignment
+    printf("Enter number of elements: %d\n", n);  //printf function calling to print Enter number of elements
+  
+    // Dynamically allocate memory using calloc() 
+    ptr = (int*)calloc(n, sizeof(int)); 
+  
+    // Check if the memory has been successfully 
+    // allocated by calloc or not 
+    if (ptr == NULL) 
+	{ 
+        printf("Memory not allocated.\n"); //Printf function calling to print Memory not allocated.
+        exit(0); 
+    } 
+    else 
+	{ 
+  
+        // Memory has been successfully allocated 
+        printf("Memory successfully allocated using calloc.\n"); //printf function calling to print Memory successfully allocated using calloc
+  
+        // Get the elements of the array 
+        for (i = 0; i < n; ++i)
+		{ 
+            ptr[i] = i + 1; 
+        } 
+  
+        // Print the elements of the array 
+        printf("The elements of the array are: "); //printf function calling to print The elements of the array are
+        for (i = 0; i < n; ++i)
+		{ 
+            printf("%d, ", ptr[i]); 
+        } 
+    } 
+  
+    return 0; //Return statement
+} //End of main function body
