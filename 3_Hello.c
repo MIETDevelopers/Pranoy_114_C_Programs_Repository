9e60c86b4aebf1d3e7882d3fee70f2729a51bfde
Pranoy-Bhan <2020a1r114@mietjammu.in>
2021-03-23 01:47:09 +0530

    file updated

---
+++ 3_Hello.c
@@ -0,0 +1,13 @@
+//Author Name: Pranoy Bhan
+//Creation Date: 16 March 2021
+//Purpose: To print hello world program
+
+#include<stdio.h>	//Preprocessor directive to include standard input output header file
+int main()	
+{	//Start of main body
+	
+	printf("HELLO WORLD");	//printing hello world
+
+	return 0;	//Return Statement
+}
+//End of main body
