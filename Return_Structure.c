8b06236212cb528c2e9f4a27e80a121012bfbceb
Pranoy-Bhan <2020a1r114@mietjammu.in>
2021-04-01 23:28:45 +0530

    Return_Structure added

---
+++ Return_Structure.c
@@ -0,0 +1,12 @@
+#include<stdio.h> //Including standard input ouput header files from library.
+struct s //structure declaration
+{
+	char text[50]; //member variable
+};
+int main()
+{	//start of main function body	
+
+	struct s a[10];	
+	printf("How are you doing");//Here we entered the texted to be printed.
+	return 's';
+}	//end of main function body
